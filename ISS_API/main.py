54279c9c099d218776ddde44ea031b4af3b65305
mhNi00 <michalnawarecki@wp.pl>
2021-04-20 20:43:59 +0200

    Responsive working script

--- ISS_API/main.py
+++ ISS_API/main.py
@@ -1,1 +1,13 @@
-print("Hello world")
+import requests
+import json
+response = requests.get("https://api.wheretheiss.at/v1/satellites/25544")
+
+def jprint(obj):
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    return text
+
+obj = response.json()
+obj = jprint(obj)
+print("Current ISS information")
+print(obj)
+
