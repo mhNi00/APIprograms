6512cc1998eab9538df8c4d05c852b787fb58499
mhNi00 <michalnawarecki@wp.pl>
2021-04-23 14:43:33 +0200

    Change to make the program not end untill you type so

--- ISS_API/main.py
+++ ISS_API/main.py
@@ -1,13 +1,18 @@
 import requests
 import json
 response = requests.get("https://api.wheretheiss.at/v1/satellites/25544")
-
+key = 's'
 def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     return text
 
-obj = response.json()
-obj = jprint(obj)
 print("Current ISS information")
-print(obj)
+while True:
+    obj = response.json()
+    obj = jprint(obj)
+    print(obj)
+    print("Press x to stop the program or s to retrieve information from ISS again")
+    key = input()
+    if key == 'x':
+        break
 
