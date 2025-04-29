f12b0a91d4ef0facda7f82fd4c1af12d6fac98eb
mhNi00 <45311458+mhNi00@users.noreply.github.com>
2025-04-29 07:16:08 +0200

    Update main.py

--- VirusThreatCheck/main.py
+++ VirusThreatCheck/main.py
@@ -3,7 +3,7 @@ import json
 import re
 url = 'https://www.virustotal.com/vtapi/v2/file/report'
 key = input("Insert the hash of the file you want to check, and i'll give you info on how many different vendors it was detected as harmful!")
-params = {'apikey': '0d96614a0b897c0594bee0bfb88c236a65027ddc45bb8d7f30a379341e1ddf16', 'resource': key}
+params = {'apikey': '', 'resource': key}
 sum_of_detects = 0
 response = requests.get(url, params)
 lib_of_vendors = ['Bkav','Elastic','MicroWorld-eScan','CMC','CAT-QuickHeal','Qihoo-360','ALYac','Cylance','Zillya','AegisLab','Sangfor','CrowdStrike','Alibaba','K7GW','K7AntiVirus','Baidu','Cyren','Symantec','ESET-NOD32','APEX','Avast','ClamAV','Kaspersky','BitDefender','NANO-Antivirus','ViRobot','Tencent','Ad-Aware','TACHYON','Emsisoft','Comodo','F-Secure','DrWeb','VIPRE','TrendMicro','FireEye','Sophos','SentinelOne','GData','Jiangmin','Webroot','Avira','Kingsoft','Gridinsoft','Arcabit','SUPERAntiSpyware','ZoneAlarm','Microsoft','Cynet','AhnLab-V3','Acronis','McAfee','MAX','VBA32','Malwarebytes','Panda','Zoner','TrendMicro-HouseCall','Rising','Yandex','Ikarus','eGambit','Fortinet','BitDefenderTheta','AVG','Cybereason','Paloalto','MaxSecure']
